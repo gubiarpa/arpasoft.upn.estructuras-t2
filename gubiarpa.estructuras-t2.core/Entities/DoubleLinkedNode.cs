9fdeb8181c31096c75a0295a5464194aff8b1b17
Billy Arredondo <billy.arredondo@pucp.pe>
2022-11-06 21:50:38 -0500

    Create Core Project with DoubleLinkedNode

---
+++ gubiarpa.estructuras-t2.core/Entities/DoubleLinkedNode.cs
@@ -0,0 +1,24 @@
+﻿namespace gubiarpa.estructuras_t2.core.Entities
+{
+    public class DoubleLinkedNode<T>
+    {
+        #region Attributes
+        private T? _element;
+        private DoubleLinkedNode<T>? _previous;
+        public DoubleLinkedNode<T>? _next;
+        #endregion
+
+        #region Constructors
+        public DoubleLinkedNode(T element)
+        {
+            _element = element;
+        }
+
+        public DoubleLinkedNode(T element, DoubleLinkedNode<T> previous, DoubleLinkedNode<T> next) : this(element)
+        {
+            _previous = previous;
+            _next = next;
+        }
+        #endregion
+    }
+}
